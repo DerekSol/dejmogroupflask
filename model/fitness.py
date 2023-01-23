6a80cd7c791df5c6fb8dd6d2ad7f3672b8d5098d
Martynguyen64 <martinsupercell06@gmail.com>
2023-01-23 01:13:52 -0800

    fitness

--- model/fitness.py
+++ model/fitness.py
@@ -12,7 +12,7 @@ from flask_login import UserMixin
 ''' Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along '''
 
 # Define the Post class to manage actions in 'posts' table,  with a relationship to 'users' table
-class fitness(UserMixin,db.Model):
+class fitness(db.Model):
     __tablename__ = 'fitness_users'
 
     # Define the Notes schema
@@ -79,130 +79,6 @@ class fitness(UserMixin,db.Model):
 # -- a.) db.Model is like an inner layer of the onion in ORM
 # -- b.) User represents data we want to store, something that is built on db.Model
 # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL
-class User(db.Model):
-    __tablename__ = 'users'  # table name is plural, class name is singular
-
-    # Define the User schema with "vars" from object
-    id = db.Column(db.Integer, primary_key=True)
-    _name = db.Column(db.String(255), unique=False, nullable=False)
-    _uid = db.Column(db.String(255), unique=True, nullable=False)
-    _password = db.Column(db.String(255), unique=False, nullable=False)
-    _dob = db.Column(db.Date)
-
-    # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
-    posts = db.relationship("Post", cascade='all, delete', backref='users', lazy=True)
-
-    # constructor of a User object, initializes the instance variables within object (self)
-    def __init__(self, name, uid, password="123qwerty", dob=date.today()):
-        self._name = name    # variables with self prefix become part of the object, 
-        self._uid = uid
-        self.set_password(password)
-        self._dob = dob
-
-    # a name getter method, extracts name from object
-    @property
-    def name(self):
-        return self._name
-    
-    # a setter function, allows name to be updated after initial object creation
-    @name.setter
-    def name(self, name):
-        self._name = name
-    
-    # a getter method, extracts email from object
-    @property
-    def uid(self):
-        return self._uid
-    
-    # a setter function, allows name to be updated after initial object creation
-    @uid.setter
-    def uid(self, uid):
-        self._uid = uid
-        
-    # check if uid parameter matches user id in object, return boolean
-    def is_uid(self, uid):
-        return self._uid == uid
-    
-    @property
-    def password(self):
-        return self._password[0:10] + "..." # because of security only show 1st characters
-
-    # update password, this is conventional setter
-    def set_password(self, password):
-        """Create a hashed password."""
-        self._password = generate_password_hash(password, method='sha256')
-
-    # check password parameter versus stored/encrypted password
-    def is_password(self, password):
-        """Check against hashed password."""
-        result = check_password_hash(self._password, password)
-        return result
-    
-    # dob property is returned as string, to avoid unfriendly outcomes
-    @property
-    def dob(self):
-        dob_string = self._dob.strftime('%m-%d-%Y')
-        return dob_string
-    
-    # dob should be have verification for type date
-    @dob.setter
-    def dob(self, dob):
-        self._dob = dob
-    
-    @property
-    def age(self):
-        today = date.today()
-        return today.year - self._dob.year - ((today.month, today.day) < (self._dob.month, self._dob.day))
-    
-    # output content using str(object) in human readable form, uses getter
-    # output content using json dumps, this is ready for API response
-    def __str__(self):
-        return json.dumps(self.read())
-
-    # CRUD create/add a new record to the table
-    # returns self or None on error
-    def create(self):
-        try:
-            # creates a person object from User(db.Model) class, passes initializers
-            db.session.add(self)  # add prepares to persist person object to Users table
-            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
-            return self
-        except IntegrityError:
-            db.session.remove()
-            return None
-
-    # CRUD read converts self to dictionary
-    # returns dictionary
-    def read(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "uid": self.uid,
-            "dob": self.dob,
-            "age": self.age,
-            "posts": [post.read() for post in self.posts]
-        }
-
-    # CRUD update: updates user name, password, phone
-    # returns self
-    def update(self, name="", uid="", password=""):
-        """only updates values with length"""
-        if len(name) > 0:
-            self.name = name
-        if len(uid) > 0:
-            self.uid = uid
-        if len(password) > 0:
-            self.set_password(password)
-        db.session.commit()
-        return self
-
-    # CRUD delete: remove self
-    # None
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-        return None
-
 
 """Database Creation and Testing """
 
@@ -213,11 +89,11 @@ def initUsers():
     """Create database and tables"""
     db.create_all()
     """Tester data for table"""
-    u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11))
-    u2 = User(name='Nicholas Tesla', uid='niko', password='123niko')
-    u3 = User(name='Alexander Graham Bell', uid='lex', password='123lex')
-    u4 = User(name='Eli Whitney', uid='whit', password='123whit')
-    u5 = User(name='John Mortensen', uid='jm1021', dob=date(1959, 10, 21))
+    u1 = fitness(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11))
+    u2 = fitness(name='Nicholas Tesla', uid='niko', password='123niko')
+    u3 = fitness(name='Alexander Graham Bell', uid='lex', password='123lex')
+    u4 = fitness(name='Eli Whitney', uid='whit', password='123whit')
+    u5 = fitness(name='John Mortensen', uid='jm1021', dob=date(1959, 10, 21))
 
     users = [u1, u2, u3, u4, u5]
 
