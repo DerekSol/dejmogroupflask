754be41cd313147f87786cd749be115ef2f95a05
Martynguyen64 <martinsupercell06@gmail.com>
2023-01-22 19:42:47 -0800

    mort

--- api/workout.py
+++ api/workout.py
@@ -2,13 +2,13 @@ from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource # used for REST API building
 from datetime import datetime
 
-from model.users import User
+from model.fitness import User
 
-user_api = Blueprint('user_api', __name__,
-                   url_prefix='/api/users')
+workout_api = Blueprint('workout_api', __name__,
+                   url_prefix='/api/workout')
 
 # API docs https://flask-restful.readthedocs.io/en/latest/api.html
-api = Api(user_api)
+api = Api(workout_api)
 
 class UserAPI:        
     class _Create(Resource):
