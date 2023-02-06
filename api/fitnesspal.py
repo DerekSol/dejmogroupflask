752d2f1736f68c44d6b4929eccd07b3a3bd87870
ethan2806 <truong.ethan@yahoo.com>
2023-02-06 11:37:45 -0800

    asd

--- api/fitnesspal.py
+++ api/fitnesspal.py
@@ -1,63 +1,25 @@
-import requests
-import json
+def macro_calculator(weight, protein_ratio, fat_ratio, carb_ratio):
+    protein_grams = weight * protein_ratio
+    fat_grams = weight * fat_ratio
+    carb_grams = weight * carb_ratio
+    calories = (protein_grams * 4) + (fat_grams * 9) + (carb_grams * 4)
+    sugar = carb_grams / 4
+    print("Protein: ", protein_grams, "g")
+    print("Fat: ", fat_grams, "g")
+    print("Carbs: ", carb_grams, "g")
+    print("Calories: ", calories, "cal")
+    print("Sugar: ", sugar, "g")
 
-# Define a class for Body Weight Loss Calculator
-class BodyWeightLossCalculator:
-    # Initialize instance variables for gender, age, weight, height, activity_level, goal_weight
-    def __init__(self, gender, age, weight, height, activity_level, goal_weight):
-        self.gender = gender
-        self.age = age
-        self.weight = weight
-        self.height = height
-        self.activity_level = activity_level
-        self.goal_weight = goal_weight
+# example usage
+weight = 70
+protein_ratio = 0.3
+fat_ratio = 0.3
+carb_ratio = 0.4
+macro_calculator(weight, protein_ratio, fat_ratio, carb_ratio)
 
-    # Method to calculate Basal Metabolic Rate (BMR)
-    def calculate_bmr(self):
-        # Check if gender is male or female and calculate BMR accordingly
-        if self.gender == 'male':
-            bmr = 66 + (6.23 * self.weight) + (12.7 * self.height) - (6.8 * self.age)
-        else:
-            bmr = 655 + (4.35 * self.weight) + (4.7 * self.height) - (4.7 * self.age)
-        # Return calculated BMR
-        return bmr
-
-    # Method to calculate Total Daily Energy Expenditure (TDEE)
-    def calculate_tdee(self):
-        # Check the activity level and calculate TDEE accordingly
-        if self.activity_level == 'sedentary':
-            tdee = 1.2 * self.calculate_bmr()
-        elif self.activity_level == 'lightly active':
-            tdee = 1.375 * self.calculate_bmr()
-        elif self.activity_level == 'moderately active':
-            tdee = 1.55 * self.calculate_bmr()
-        elif self.activity_level == 'very active':
-            tdee = 1.725 * self.calculate_bmr()
-        else:
-            tdee = 1.9 * self.calculate_bmr()
-        # Return calculated TDEE
-        return tdee
-
-    # Method to calculate daily caloric deficit
-    def calculate_daily_caloric_deficit(self):
-        # Calculate target TDEE and BMR
-        target_tdee = self.calculate_tdee()
-        target_weight = self.weight - (self.weight - self.goal_weight)
-        target_bmr = target_weight * 10 + self.calculate_bmr() - self.weight * 10
-        # Calculate daily caloric deficit
-        daily_caloric_deficit = target_tdee - target_bmr
-        # Return calculated daily caloric deficit
-        return daily_caloric_deficit
-
-    # Method to calculate the target macros
-    def calculate_macros(self):
-        # Define ratios for protein, fat, and carbohydrates
-        protein_ratio = 0.30
-        fat_ratio = 0.20
-        carbohydrate_ratio = 0.50
-        # Calculate daily amount of each macro
-        daily_protein = self.weight * protein_ratio
-        daily_fat = self.weight * fat_ratio
-        daily_carbohydrate = self.weight * carbohydrate_ratio
-        # Calculate daily caloric deficit
-        daily_caloric_deficit = self.calculate_daily_
+# The code defines a class BodyWeightLossCalculator that calculates various metrics related to body weight loss.
+# The class initializes the following instance variables: gender, age, weight, height, activity_level, goal_weight.
+# The class has a method calculate_bmr that calculates the Basal Metabolic Rate (BMR) of a person based on their gender, weight, height, and age.
+# The class has a method calculate_tdee that calculates the Total Daily Energy Expenditure (TDEE) of a person based on their activity level and BMR.
+# The class has a method calculate_daily_caloric_deficit that calculates the daily caloric deficit required to achieve a person's goal weight.
+# The class has a method calculate_macros that calculates the daily amounts of protein, fat, and carbohydrates required to achieve a person's goal weight.
