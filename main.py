d97916362e7fdeca93a010c943966d10ab72198b
DerekSol <dmsol218@gmail.com>
2023-02-26 14:13:25 -0800

    ne

--- main.py
+++ main.py
@@ -25,15 +25,13 @@ def stub():
 @app.before_first_request
 def activate_job():
     with app.app_context():
+        db.init_app(app)
         db.create_all()
         print("test")
-        initUsers()
-        initUsers()
         init_diets()
 # this runs the application on the development server
 if __name__ == "__main__":
-    db.init_app(app)
     # change name for testing
     from flask_cors import CORS
     cors = CORS(app)
-    app.run(debug=True, host="0.0.0.0", port="8086")
+    app.run(debug=True, host="0.0.0.0", port="8080")
